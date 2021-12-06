c68af035aa69a2a738ae91dbac68ff71b7964238
Masaki Yoshiiwa <yoshiiwamasaki@gmail.com>
2021-12-06 19:55:28 +0900

    Fix some build error & bugs

--- front-end/lib/api/http.ts
+++ front-end/lib/api/http.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: process.env.API_ENDPOINT || 'http://localhost:8000',
+  baseURL: process.env?.API_ENDPOINT || 'http://localhost:8000',
   headers: {
     "Content-type": "application/json",
   },
