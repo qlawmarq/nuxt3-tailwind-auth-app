be0720e9be3f555eb97ae207a913f295b11d1e96
qlawmarq <masaki.yoshiiwa@gmail.com>
2023-07-25 10:43:37 +0900

    fix: add margin auto to center card (#15)

--- components/atoms/div/Card.vue
+++ components/atoms/div/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
+  <div class="bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4 min-w-[400px]">
     <slot />
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container flex justify-center items-center p-4">
+  <div class="container flex justify-center items-center p-4 m-auto">
     <slot />
   </div>
 </template>
